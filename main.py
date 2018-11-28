7fc2aad9877fe72c1b2c50e8aea5c46ed2914ac7
Reza Shah <33800875+iamrezashah@users.noreply.github.com>
2018-11-28 22:44:14 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+# By Reza Shah.
+# Instagram: Phika.ir
+
+import kivy
+kivy.require("1.10.1")
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+
+
+class CalcGridLayout(GridLayout):
+
+    def calculate(self, calculation):
+        if calculation:
+            try:
+                self.display.text = str(eval(calculation))
+            except Exception:
+                self.display.text = "Error !"
+
+
+class CalculatorApp(App):
+    def build(self):
+        return CalcGridLayout()
+
+
+calcApp = CalculatorApp()
+calcApp.run()
